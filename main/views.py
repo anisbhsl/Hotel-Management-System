3ed838690594866b569dec16b8bbe4ed3c8659e3
bhusal <bhusal.anish12@gmail.com>
2018-01-04 00:07:36 +0545

    Index template added

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 
 # Create your views here.
 # TODO Create views properly
 def index(request):
-    return HttpResponse('Successfully completed first app creation')
+    return render(request,'main/base.html')
