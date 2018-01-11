af268c60b8b24f01bfd7fb96cad95f445a470fec
Krishna Upadhyay <072bct517krishna@ioe.edu.np>
2018-01-11 08:26:13 +0545

    views

--- main/views.py
+++ main/views.py
@@ -4,5 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 # TODO Create views properly
 def index(request):
-    hello = "Hello"
-    return render(request, 'main/index.html', context={'hello_name':hello})
+    message = "It is working."
+    page_title = "Hotel Management System"
+    return render(
+        request,
+        'main/index.html',
+        context={
+            'message': message,
+            'title': page_title,
+        }
+    )
