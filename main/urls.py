645f124e28597a79e54ad319d42463bbabea8e52
Krishna Upadhyay <072bct517krishna@ioe.edu.np>
2018-01-12 13:31:53 +0545

    some changes regarding to front end

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views import generic
 
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('rooms/', views.RoomListView.as_view(), name='rooms'),
 ]
